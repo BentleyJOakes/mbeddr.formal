e1102c713b65dd9eace97fa33493eab9dfdac9f1
bentleyjoakes <bentleyjoakes@gmail.com>
2020-06-12 14:58:39 +0200

    Add JBR to MPS.

--- .github/workflows/download_mps.sh
+++ .github/workflows/download_mps.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 MPS_ZIP="https://download.jetbrains.com/mps/2019.3/MPS-2019.3.4.zip"
+JBR="https://bintray.com/jetbrains/intellij-jbr/download_file?file_path=jbr-11_0_7-windows-x64-b765.57.tar.gz"
 
 set -e
 
+echo "Downloading MPS"
 echo ${MPS_ZIP}
 
 if [ ! -f "MPS.zip" ]; then
@@ -13,3 +15,12 @@ fi
 
 echo "Unzipping MPS"
 unzip -o "MPS.zip" -d ~
+
+
+echo "Downloading JBR - JetBrainsRuntime"
+if [ ! -f "jbr.tar.gz" ]; then
+    echo "Downloading $JBR" 
+    curl -L ${JBR} > "jbr.tar.gz"
+fi
+
+tar -C $HOME'/MPS 2019.3/' -zxvf "jbr.tar.gz"
