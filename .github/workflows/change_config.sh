c4bae6630d9cb8f70eaadeeff384ec5eee6cc614
bentleyjoakes <bentleyjoakes@gmail.com>
2020-06-12 16:08:35 +0200

    Specify mps.home.

--- .github/workflows/change_config.sh
+++ .github/workflows/change_config.sh
@@ -7,6 +7,7 @@ echo "Changing config"
 cd ../..
 
 echo "mpsHomeDir='/home/runner/MPS 2019.3'" > gradle.properties
+echo "mps.home='/home/runner/MPS 2019.3'" > gradle.properties
 echo "mbeddr.formal.home='/home/runner/mbeddr.formal'" >> gradle.properties
 
 
